7d4a955fdc7408818d14f06ecad11d18096b1587
ItsJonQ <itsjonq@gmail.com>
2015-03-24 22:00:04 -0400

    Setup basic template/controller for sticker component

--- app/components/spx-sticker.js
+++ app/components/spx-sticker.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
+import layout from '../templates/components/spx-sticker';
 
 export default Ember.Component.extend({
+  layout: layout,
+  tagName: 'div',
+  classNames: ['stickerpax__sticker'],
+  data: null,
+
+  hasImage: Ember.computed.notEmpty('data.image')
 });
---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  data: [
+    {
+      name: 'sticker',
+      image: 'image.jpg'
+    }
+  ]
+});
