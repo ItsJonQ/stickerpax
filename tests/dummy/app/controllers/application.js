d170a05d3bfdc38934a7c101af4260e3c248687c
ItsJonQ <itsjonq@gmail.com>
2015-03-24 22:21:37 -0400

    Adjusted fake data in application.js

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -3,8 +3,24 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   data: [
     {
-      name: 'sticker',
-      image: 'image.jpg'
+      name: 'sticker1',
+      image: 'image1.jpg',
+      rarity: 1
+    },
+    {
+      name: 'sticker2',
+      image: 'image2.jpg',
+      rarity: 1
+    },
+    {
+      name: 'sticker3',
+      image: 'image3.jpg',
+      rarity: 2
+    },
+    {
+      name: 'sticker4',
+      image: 'image4.jpg',
+      rarity: 3
     }
   ]
 });
